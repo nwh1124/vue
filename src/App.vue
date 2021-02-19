bf67ee4ac8f43469780053ec767e5f462e82bb57
nwh1124 <nwh0716@gmail.com>
2021-02-19 17:51:34 +0900

    쓸 데 없는 파일 제거

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci corrupti, neque voluptate totam omnis voluptatibus nihil vero cupiditate vel tenetur, porro accusamus ipsa, necessitatibus nobis minus soluta excepturi ut repudiandae.
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'App'
+})
+</script>
+
+<style>
+html, body {
+  margin:0;
+}
+
+#app{
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale; 
+}
+</style>
