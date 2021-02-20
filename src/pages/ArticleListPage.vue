75355e1d7eeb35e50546470e37e31f517904d3b6
nwh1124 <nwh0716@gmail.com>
2021-02-20 10:59:06 +0900

    FormRow 입력폼 구현, 폰트 적용, 상황별 버튼 구현

--- src/pages/ArticleListPage.vue
+++ src/pages/ArticleListPage.vue
@@ -1,6 +1,27 @@
 <template>
   <TitleBar>게시물 리스트</TitleBar>
-  <input type="checkbox" />
+  <section class="article-wrtie-form-box">
+    <div class="container mx-auto">
+      <form action="">
+        <FormRow title="제목">
+          <input class="form-row-input" type="text" placeholder="제목을 입력해주세요." />
+        </FormRow>
+        <FormRow title="내용">
+          <textarea class="form-row-input" placeholder="내용을 입력해주세요."></textarea>
+        </FormRow>
+        <FormRow title="작성">
+          <div class="btns">
+            <input type="button" value="작성" class="btn-primary" />
+            <input type="button" value="작성" class="btn-secondary" />
+            <input type="button" value="작성" class="btn-success" />
+            <input type="button" value="작성" class="btn-danger" />
+            <input type="button" value="작성" class="btn-warning" />
+            <input type="button" value="작성" class="btn-info" />
+          </div>
+        </FormRow>
+      </form>
+    </div>
+  </section>
 </template>
 
 <script lang="ts">
